d9cf50d602527342fd497ebc1bf4cd6ce842887d
Aurélien Deutsch <Aurlien Deutsch>
2016-09-12 00:49:36 +0200

    Project

--- PouletGame/PouletGame.java
+++ PouletGame/PouletGame.java
@@ -1,6 +1,19 @@
+import java.util.Scanner; 
+
 
 class PouletGame {
    public static void main (String[] args){
-    System.out.println("Welcome in PouletGame !");
+    System.out.println("Welcome in PouletGame !\n\nEtes-vous un homme(1) ou une femme(0) ?");
+	
+	Scanner sc1 = new Scanner(System.in); 
+	int sexe = sc1.nextInt(); 
+	
+	System.out.println("Quel est votre nom de personnage ?");
+	
+	Scanner sc2 = new Scanner(System.in); 
+	String nom = sc2.next();
+	
+	Personnage heros = new Personnage(sexe, nom);
+	heros.LinkString();
    }
 }
