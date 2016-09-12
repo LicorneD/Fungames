fe29e1b275bc61c8ca485d6bee5717225bbe1179
Rekwyn <clementm.nerdy@gmail.com>
2016-09-12 11:42:21 +0200

    modifMonstreJava

--- PouletGame/Monstre.java
+++ PouletGame/Monstre.java
@@ -2,28 +2,17 @@
 
 // Monster class
 public class Monstre{
-	int pv;
-	int power;
 	String nom;
 	
 	// Monster constructor
-	public Monstre(int xpv, int xpower, String xnom){
-		pv = xpv;
-		power = xpower;
+	public Monstre(String xnom){
 		nom = xnom;
 		}
 	
 	// Monster appears method
 	public void MonsterAppears(){
 	String text1 = "Un " + nom + "vient d'apparaître devant vous !";
-	String text2 = "";
-	if (power >= 10){
-		text2 = "... on dirait qu'il veut vous démembrer, il a l'air fort !";
-	}
-	else{
-		text2 = "... et il a une tête de rat crevé ! Le tuer ne posera pas trop de problème...";
-	}
-	System.out.println(text1 + text2);
+	System.out.println(text1);
 	}
 
 
