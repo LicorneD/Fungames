7c3a167866fa7e9e8cc696b90d283aba96ac9123
Rekwyn <clementm.nerdy@gmail.com>
2016-09-12 12:54:19 +0200

    retest

---
+++ PouletGame/Renard.java
@@ -0,0 +1,21 @@
+// Deuxieme essai
+
+// Classe renard
+public class Renard extends Monstre{
+
+	// Constructeur
+	public Renard(String xnom){
+	super(xnom);
+	puissance = (int)(Math.random() * (20-10) + 10);
+	vie = (int)(Math.random() * (20-10) + 10);
+	}
+
+	public String OmgUnMonstre(){
+	String text1 = super.OmgUnMonstre() + " est apparu devant vous, c'est un renard de puissance de " + puissance + "et avec" + vie + "pv";
+	System.out.println(text1);
+	return text1;
+	}
+}
+
+
+
